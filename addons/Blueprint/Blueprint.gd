80a91d32c4cfc64e84edb3ea22ce2ceb8ec3fbec
phosxd <phosphorusxd@proton.me>
2025-06-16 14:25:29 -0400

    More validation.
    - Added validation for "optional", "default", & "range" parameters in Blueprints.
    
    Next:
    - Add "enum", "prefix", "suffix", "regex", & "element_types" parameter validation.
    - Implement "enum" parameter functionality.

--- addons/Blueprint/Blueprint.gd
+++ addons/Blueprint/Blueprint.gd
@@ -8,6 +8,9 @@ enum error {
 	ERR_BP_PARAMSET_NOT_DICTIONARY,
 	ERR_BP_PARAMSET_MISSING_TYPE,
 	ERR_BP_PARAMSET_INVALID_TYPE_PARAM,
+	ERR_BP_PARAMSET_INVALID_OPTIONAL_PARAM,
+	ERR_BP_PARAMSET_INVALID_DEFAULT_PARAM,
+	ERR_BP_PARAMSET_INVALID_RANGE_PARAM,
 }
 ## Blueprint errors as explanatory strings.
 const error_strings:Array[String] = [
@@ -16,6 +19,9 @@ const error_strings:Array[String] = [
 	'Blueprint parameter set must be of type `Dictionary`.',
 	'Blueprint parameter set must contain a "type" parameter.',
 	'Blueprint parameter set\'s "type" parameter must be of type `String` & match one of the following values: "string", "int", "float", "array", "dict", ">{blueprint_name}".',
+	'Blueprint parameter set\'s "optional" parameter must be of type `bool`.',
+	'Blueprint parameter set\'s "default" parameter value type must match the `type` parameter.',
+	'Blueprint parameter set\'s "range" parameter must be of type `Array` & have 2 elements of type `int`.',
 ]
 ## Blueprint data.
 var data:Dictionary
@@ -26,13 +32,15 @@ var valid:bool
 
 ## Initializes the Blueprint.
 func _init(name:String, data:Dictionary) -> void:
-	self.data = data
-	var validation:error = _validate(data)
-	if validation != error.OK: 
+	# If invalid Blueprint, do nothing.
+	var validation_error:error = _validate(data)
+	if validation_error: 
 		valid = false
-		push_error(error_strings[validation])
+		push_error(error_strings[validation_error])
 		return
 	valid = true
+	# Otherwise, set up Blueprint.
+	self.data = data
 	BlueprintManager.add_blueprint(name, self)
 
 
@@ -41,18 +49,46 @@ static func _validate(data:Dictionary) -> error:
 	for key in data:
 		var value = data[key]
 		if typeof(value) != TYPE_DICTIONARY: return error.ERR_BP_PARAMSET_NOT_DICTIONARY
-		# Get type.
+
+		# Validate "type" parameter.
 		var type_param = value.get('type')
 		if type_param == null: return error.ERR_BP_PARAMSET_MISSING_TYPE
 		if typeof(type_param) != TYPE_STRING: return error.ERR_BP_PARAMSET_INVALID_TYPE_PARAM
+		var type_param_literal_type:Variant.Type # Track the type, for use in other checks.
 		match type_param:
-			'string': pass
-			'int': pass
-			'float': pass
-			'array': pass
-			'dict': pass
+			'string': type_param_literal_type = TYPE_STRING
+			'int': type_param_literal_type = TYPE_INT
+			'float': type_param_literal_type = TYPE_FLOAT
+			'array': type_param_literal_type = TYPE_ARRAY
+			'dict': type_param_literal_type = TYPE_DICTIONARY
 			_:
 				if not type_param.begins_with('>'): return error.ERR_BP_PARAMSET_INVALID_TYPE_PARAM
+				type_param_literal_type = TYPE_NIL
+
+		# Validate "optional" parameter.
+		var optional_param = value.get('optional')
+		if optional_param != null && typeof(optional_param) != TYPE_BOOL: return error.ERR_BP_PARAMSET_INVALID_OPTIONAL_PARAM
+
+		# Validate "default" parameter.
+		var default_param = value.get('default')
+		var typeof_default_param := typeof(default_param)
+
+		# Set typeof_default_param to `int` if holds no floating value.
+		if typeof_default_param == TYPE_FLOAT:
+			if round(default_param) == default_param: typeof_default_param = TYPE_INT
+		if typeof_default_param != type_param_literal_type: return error.ERR_BP_PARAMSET_INVALID_DEFAULT_PARAM
+
+		# Validate "range" parameter.
+		var range_param = value.get('range')
+		if range_param != null:
+			if typeof(range_param) != TYPE_ARRAY: return error.ERR_BP_PARAMSET_INVALID_RANGE_PARAM
+			var count:int = 0
+			for item in range_param:
+				if typeof(item) not in [TYPE_INT, TYPE_FLOAT]: return error.ERR_BP_PARAMSET_INVALID_RANGE_PARAM
+				if round(item) != item: return error.ERR_BP_PARAMSET_INVALID_RANGE_PARAM
+				count += 1
+			if count != 2: return error.ERR_BP_PARAMSET_INVALID_RANGE_PARAM
+
 	return error.OK
 
 
